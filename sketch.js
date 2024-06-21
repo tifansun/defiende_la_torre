616aaa7448cd775237e2f892e413c65404c7fca7
tifansun <tifansun@gamil.com>
2024-06-21 16:52:36 -0600

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(1200, 600);
+
+}
+
+function draw() {
+
+}
